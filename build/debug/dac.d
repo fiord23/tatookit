c0209afc7aa1a07b11d08f4119569c09a527b868
d.zaretski <d.zaretski@atomgroup.io>
2025-01-30 15:16:45 +0300

    added untracked files

---
+++ build/debug/dac.d
@@ -0,0 +1,14 @@
+build/debug/dac.o: Core/Src/dac.c Core/Inc/dac.h \
+ Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l431xx.h \
+ Drivers/CMSIS/Include/core_cm4.h Drivers/CMSIS/Include/cmsis_version.h \
+ Drivers/CMSIS/Include/cmsis_compiler.h Drivers/CMSIS/Include/cmsis_gcc.h \
+ Drivers/CMSIS/Include/mpu_armv7.h \
+ Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+Core/Inc/dac.h:
+Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l431xx.h:
+Drivers/CMSIS/Include/core_cm4.h:
+Drivers/CMSIS/Include/cmsis_version.h:
+Drivers/CMSIS/Include/cmsis_compiler.h:
+Drivers/CMSIS/Include/cmsis_gcc.h:
+Drivers/CMSIS/Include/mpu_armv7.h:
+Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
